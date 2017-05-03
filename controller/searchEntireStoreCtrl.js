7a81140c591342a84d19a1487634cc58a8ffb8e0
MajdLetaief <majdeddine.letaief@esprit.tn>
2017-05-03 02:28:03 +0100

    updating views

--- controller/searchEntireStoreCtrl.js
+++ controller/searchEntireStoreCtrl.js
@@ -1,54 +1,27 @@
+app.controller('DemoCtrl', ['$scope', '$http', "$location", "$timeout", "$q", "$log",
 
-
-    app.controller('DemoCtrl', ['$scope','$http',"$location","$timeout", "$q", "$log", function($scope,$http,$location) {
+    function ($scope, $http, $location) {
         var self = this;
         self.simulateQuery = false;
-        self.isDisabled    = false;
-
-        // list of `state` value/display objects
-        self.states        = loadAll();
-        self.querySearch   = querySearch;
+        self.isDisabled = false;
+        self.states = loadAll();
+        self.querySearch = querySearch;
         self.selectedItemChange = selectedItemChange;
-        self.searchTextChange   = searchTextChange;
-
+        self.searchTextChange = searchTextChange;
         self.newState = newState;
 
         function newState(state) {
             alert("Sorry! You'll need to create a Constitution for " + state + " first!");
         }
 
-        // ******************************
-        // Internal methods
-        // ******************************
-
-        /**
-         * Search for states... use $timeout to simulate
-         * remote dataservice call.
-
-        function querySearch (query) {
-            console.log(query);
-            var results = self.states.$$state.value.filter( function(){
-                    var arr = [];
-                    angular.forEach(self.states.$$state.value, function(v){
-                        if(v.name.toLowerCase() === query.toLowerCase()){
-                            console.log("add to tabel");
-                            arr.push(v);
-                        }
-                    });
-
-                    console.log(arr);
-                    return arr;
-
-            });
-            console.log(results);
-            return results;
-        } */
-        function querySearch (query) {
-            var results = query ? self.states.$$state.value.filter( createFilterFor(query) ) : self.states.$$state.value,
+        function querySearch(query) {
+            var results = query ? self.states.$$state.value.filter(createFilterFor(query)) : self.states.$$state.value,
                 deferred;
             if (self.simulateQuery) {
                 deferred = $q.defer();
-                $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+                $timeout(function () {
+                    deferred.resolve(results);
+                }, Math.random() * 1000, false);
                 return deferred.promise;
             } else {
                 return results;
@@ -56,12 +29,12 @@
         }
 
         function searchTextChange(text) {
-           //todo
+            //todo
         }
 
         function selectedItemChange(item) {
-            console.log( JSON.stringify(item.productId));
-            $location.path("/product/"+item.productId);
+            console.log(JSON.stringify(item.productId));
+            $location.path("/product/" + item.productId);
 
         }
 
@@ -69,12 +42,12 @@
          * Build `states` list of key/value pairs
          */
         function loadAll() {
-           return $http.get("https://showroomercore.mybluemix.net/api/product/getall").then(function (response) {
-               var productsName;
-               $scope.products = response.data;
-               return $scope.products;
+            return $http.get("https://showroomercore.mybluemix.net/api/product/getall").then(function (response) {
+                var productsName;
+                $scope.products = response.data;
+                return $scope.products;
 
-           });
+            });
         }
 
         /**
